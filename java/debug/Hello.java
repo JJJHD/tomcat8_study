3167d12ca0346ef2a4a845117292b8acf738b48f
1198835488@qq.com <jhdgit199312>
2020-02-04 17:13:55 +0800

    commit

--- java/debug/Hello.java
+++ java/debug/Hello.java
@@ -4,6 +4,48 @@ package debug;
  * 	Bootstrap
  *	CoyoteAdapter
  *
+ *	SingleThreadModel	
+ *	
+ *	RequestFacade
+ *
+ *	HostConfig
+ *
+ *	HttpServlet
+ *
+ *	Container
+ *
+ *	StandardWrapperValve
+ *
+ *	valve : ∑ß√≈
+ *
+ *	RequestFilterValve
+ *
+ *	JIoEndpoint
+ *
+ *	NioEndpoint
+ *
+ *	Http11Protocol
+ *
+ *	Http11NioProtocol
+ *
+ *	Connector
+ *
+ *	AbstractProtocol
+ *
+ *	AbstractHttp11Processor
+ *
+ *	InternalInputBuffer
+ *
+ *	WebappLoader
+ *
+ *	WebappClassLoaderBase
+ *	
+ *engine
+ *host
+ *context
+ *wrapper
+ *servlet
+ *
  */
 
 public class Hello {
